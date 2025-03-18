dde6204542a821ed5bbb1134c8438f592397cd1b
steph ridnell <steph.ridnell@gmail.com>
2025-03-18 22:15:13 +1100

    Removed comments

--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ from textnode import TextNode, TextType
 
 def main():
     print("# hello world")
-    text_node = TextNode("hello world", TextType.LINK,
-                         "https://www.google.com")
-    print(text_node)
 
 
 if __name__ == "__main__":
