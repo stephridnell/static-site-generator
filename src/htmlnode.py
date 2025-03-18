6527e9c41d9b89695e5a91454553b9d67bf999df
steph ridnell <steph.ridnell@gmail.com>
2025-03-18 21:15:29 +1100

    Added html node class and tests

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HtmlNode:
+    def __init__(self, tag: str | None = None, value: str | None = None,
+                 children: list['HtmlNode'] | None = None, props: dict | None = None) -> None:
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError("to_html not implemented")
+
+    def props_to_html(self) -> str:
+        if self.props is None:
+            return ""
+        return " " + " ".join([f"{k}={v}" for k, v in self.props.items()])
+
+    def __repr__(self) -> str:
+        return f"HtmlNode({self.tag}, {self.value}, {self.children}, {self.props})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,68 @@
+import unittest
+
+from htmlnode import HtmlNode
+
+
+class TestHtmlNode(unittest.TestCase):
+    def test_init_empty(self):
+        node = HtmlNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_init_with_tag(self):
+        node = HtmlNode("p")
+        self.assertEqual(node.tag, "p")
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_init_with_value(self):
+        node = HtmlNode(value="Hello")
+        self.assertIsNone(node.tag)
+        self.assertEqual(node.value, "Hello")
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_init_with_children(self):
+        child = HtmlNode("p", "child")
+        node = HtmlNode("div", children=[child])
+        self.assertEqual(node.tag, "div")
+        self.assertIsNone(node.value)
+        self.assertEqual(node.children, [child])
+        self.assertIsNone(node.props)
+
+    def test_init_with_props(self):
+        props = {"class": "\"my-class\"", "id": "\"my-id\""}
+        node = HtmlNode("div", props=props)
+        self.assertEqual(node.tag, "div")
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertEqual(node.props, props)
+
+    def test_props_to_html_none(self):
+        node = HtmlNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html(self):
+        props = {"class": "\"my-class\"", "id": "\"my-id\""}
+        node = HtmlNode(props=props)
+        self.assertEqual(node.props_to_html(),
+                         " class=\"my-class\" id=\"my-id\"")
+
+    def test_to_html_not_implemented(self):
+        node = HtmlNode()
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
+
+    def test_repr(self):
+        props = {"class": "\"my-class\""}
+        child = HtmlNode("p", "child")
+        node = HtmlNode("div", "parent", [child], props)
+        self.assertEqual(repr(
+            node), "HtmlNode(div, parent, [HtmlNode(p, child, None, None)], {'class': '\"my-class\"'})")
+
+
+if __name__ == "__main__":
+    unittest.main()
